4a605734c4dd248353cbbe03fec87a8b482c5920
Jude Mwenda <judemwenda@gmail.com>
2016-01-08 15:19:45 -0500

    Changes to introduce WMTS support to the GXP and its addition to the layer preview page

--- worldmap/static/worldmap/app/static/externals/gxp/src/script/plugins/WMTSSource.js
+++ worldmap/static/worldmap/app/static/externals/gxp/src/script/plugins/WMTSSource.js
@@ -8,152 +8,12 @@
 /**
  * @requires plugins/LayerSource.js
  * @requires OpenLayers/Layer/WMTS.js
+ * @requires GeoExt/data/WMTSCapabilitiesReader.js
+ * @requires GeoExt/data/WMTSCapabilitiesStore.js
  * @requires OpenLayers/Format/WMTSCapabilities/v1_0_0.js
  */
 
 Ext.ns('gxp.data', 'gxp.plugins');
-
-gxp.data.WMTSCapabilitiesReader = Ext.extend(Ext.data.DataReader, {
-    constructor: function(meta, recordType) {
-        meta = meta || {};
-        if (!meta.format) {
-            meta.format = new OpenLayers.Format.WMTSCapabilities();
-        }
-        if(typeof recordType !== "function") {
-            recordType = GeoExt.data.LayerRecord.create(
-                recordType || meta.fields || [
-                    {name: "name", type: "string"},
-                    {name: "title", type: "string"},
-                    {name: "tileMapUrl", type: "string"}
-                ]);
-        }
-        gxp.data.WMTSCapabilitiesReader.superclass.constructor.call(
-            this, meta, recordType);
-    },
-    equivalentProj: function(proj1, proj2) {
-        // Necessary because OpenLayers.Projection.equals does not handle
-        // equivalent definitions, e.g. EPSG:900913 vs urn:ogc:def:crs:EPSG::900913
-        if (proj1.length < proj2.length) {
-            var projtmp = proj1;
-            proj1 = proj2;
-            proj2 = projtmp;
-        }
-        proj1 = proj1.replace(/::/g, ":").replace(/CRS84/g, "EPSG:4326").replace(/OSGEO:41001/g, "EPSG:900913");
-        proj2 = proj2.replace(/::/g, ":").replace(/CRS84/g, "EPSG:4326").replace(/OSGEO:41001/g, "EPSG:900913");
-        console.log("proj1" + proj1);
-        console.log(proj2);
-        return (proj1.indexOf(proj2, proj1.length - proj2.length) !== -1);
-    },
-    read: function(request) {
-        var data = request.responseXML;
-        if(!data || !data.documentElement) {
-            data = request.responseText;
-        }
-        return this.readRecords(data);
-    },
-    readRecords: function(data) {
-        var records = [], i, ii, j, jj, url, proj, projStr, encoding, wrongProjCount = 0;
-        if (typeof data === "string" || data.nodeType) {
-            data = this.meta.format.read(data);
-            this.raw = data;
-            console.log("Is this it?");
-            console.log(data.contents);
-            if (data.contents) {
-               if (data.contents.layers && data.contents.tileMatrixSets) {
-               for (i=0, ii=data.contents.layers.length; i<ii; i++) {
-                 var layer=data.contents.layers[i];
-                 for (j=0, jj=layer.tileMatrixSetLinks.length; j<jj; j++) { // loop  on layer matrixSets to find compatible ones
-
-                   if (data.contents.tileMatrixSets[layer.tileMatrixSetLinks[j].tileMatrixSet] &&
-                       data.contents.tileMatrixSets[layer.tileMatrixSetLinks[j].tileMatrixSet].matrixIds.length>0){
-                         var matrixIds = data.contents.tileMatrixSets[layer.tileMatrixSetLinks[j].tileMatrixSet].matrixIds;
-                         console.log("matrixids");
-                         console.log(matrixIds);
-                         // assume all the matrices in the set have the same projection
-                         projStr = matrixIds[0].supportedCRS;
-                         //console.log(projStr);
-                         proj = new OpenLayers.Projection(projStr);
-                         //console.log(this.meta.mapProjection.equals(proj));
-                         //console.log(this.equivalentProj(projStr, this.meta.mapProjection.projCode));
-                         var style=null;
-                         if (layer.styles.length===0) {
-                             // this shouldn't happen according the standard, but some Geoserver versions
-                             // were buggy: https://jira.codehaus.org/browse/GEOS-6190
-                             style="";
-                         }
-                         else {
-                             var k, kk;
-                             for (k=0, kk=layer.styles.length; k<kk; k++) {
-                                 if (layer.styles[k].isDefault) {
-                                     style=layer.styles[k].identifier;
-                                     break;
-                                 }
-                             }
-                             if (style === null) {
-                                 // no default style, just choose the first one
-                                 style = layer.styles[0].identifier;
-                             }
-                         }
-                         var resolutions = [];
-                         var units = (this.meta.mapProjection.projCode === "EPSG:4326" ? "degrees" : "m");
-                         for (var res = 0, l = matrixIds.length; res < l; res++) {
-                             resolutions.push(
-                                 matrixIds[res].scaleDenominator * 0.28E-3 /
-                                     OpenLayers.METERS_PER_INCH /
-                                     OpenLayers.INCHES_PER_UNIT[units]);
-                         }
-
-                         var config = {
-                                 name: layer.title,
-                                 layer: layer.identifier,
-                                 requestEncoding: encoding,
-                                 url: url || this.meta.baseUrl.split("?")[0],
-                                 style: style,
-                                 matrixSet: layer.tileMatrixSetLinks[j].tileMatrixSet,
-                                 matrixIds: matrixIds
-                             };
-                             // prefer png if available
-                          for (var curFormat=0, formatCount=layer.formats.length; curFormat<formatCount; curFormat++) {
-                                 if ("image/png" === layer.formats[curFormat]) {
-                                     config["format"]=layer.formats[curFormat];
-                                 }
-                             }
-
-                             console.log(layer.bound);
-                             /*layer.addOptions({
-                               maxExtent: OpenLayers.Bounds.fromArray([-122.911,42.291,-122.787,42.398]),
-                             })*/
-                             console.log("layers");
-                             console.log(layer);
-                             records.push(new this.recordType({
-                                 /*layer: new OpenLayers.Layer.WMTS(config),*/
-                                 layer: this.meta.format.createLayer( this.raw, {matrixSet: config.matrixSet, layer: layer.identifier}),
-                                 title: layer.title,
-                                 source: null,
-                                 name: layer.identifier,
-                                 tileMapUrl: config.url,
-                                 properties: "gxp_wmslayerpanel",
-                                 formats: layer.formats,
-                                 styles: layer.styles
-                             }));
-                       }
-                 }
-
-               }
-               console.log(records);
-               }
-
-             }
-
-        }
-
-        return {
-            totalRecords: records.length,
-            success: true,
-            records: records
-        };
-    }
-});
 
 /** api: (define)
  *  module = gxp.plugins
@@ -229,8 +89,11 @@ gxp.plugins.WMTSSource = Ext.extend(gxp.plugins.LayerSource, {
      */
     createStore: function() {
         var format = this.format;
-        this.store = new Ext.data.Store({
+        cleanurl = this.getCapabilitiesUrl(this.url, this.version);
+        this.store = new GeoExt.data.WMTSCapabilitiesStore({
             autoLoad: true,
+            url: cleanurl,
+            version: this.version,
             listeners: {
                 load: function() {
                     this.title = this.getTitle(this.store.reader.raw);
@@ -244,16 +107,10 @@ gxp.plugins.WMTSSource = Ext.extend(gxp.plugins.LayerSource, {
                 scope: this
             },
             proxy: new Ext.data.HttpProxy({
-                url: this.getCapabilitiesUrl(this.url, this.version),
-                disableCaching: false,
-                method: "GET"
-            }),
-            reader: new gxp.data.WMTSCapabilitiesReader({
-                baseUrl: this.url,
-                version: this.version,
-                mapProjection: this.getMapProjection(),
-                preferredEncoding: this.preferredEncoding || undefined
-            })
+                 url: this.getCapabilitiesUrl(this.url, this.version),
+                 disableCaching: false,
+                 method: "GET"
+             })
         });
     },
 
